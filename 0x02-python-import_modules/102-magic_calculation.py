3f1412e270b7ba3149afcd46f4d331e59f44b6b7
Kimathi isaac <Isaackimathi@gmail.com>
2023-08-16 21:36:01 +0300

    python

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def magic_calculation(a, b):
+
+    from magic_calculation_102 import add, sub
+
+    if x < y:
+        z = add(x, y)
+        for q in range(4, 6):
+            z = add(z, q)
+        return (z)
+
+    else:
+        return (sub(x, y))
