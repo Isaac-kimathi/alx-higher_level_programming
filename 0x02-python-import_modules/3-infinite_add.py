b60b1ab24cee70d0e1546ba5a96219dd2228b277
Kimathi isaac <Isaackimathi@gmail.com>
2023-08-16 20:14:28 +0300

    python

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 #prints the result of the addition of all arguments
+if __name__ == "__main__":
+    import sys
 
-from sys import argv
-tgtr = 0
-for x in argv[1:]:
-    tgtr += int(x)
-    print("{:d}".format(tgtr))
+    tgtr = 0
+    for x in range(len(sys.argv) - 1):
+        tgtr += int(sys.argv[x + 1)
+    print("{}".format(tgtr))
