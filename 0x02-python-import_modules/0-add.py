82b1217d73d97508d57946a10621e10206f6de1c
Kimathi isaac <Isaackimathi@gmail.com>
2023-08-16 18:25:47 +0300

    python

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,9 +2,10 @@
 
 # imports the function def add(a, b): from the file add_0.py and
 # prints the result of the addition 1 + 2 = 3
-from add_0 import add
+if __name__ == "__main__":
+    from add_0 import add
 
-a = 1
-b = 2
+    a = 1
+    b = 2
 
-print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
