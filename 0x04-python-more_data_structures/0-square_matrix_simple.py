952729380c0d3c1b2c5bd76aa1b3b554f438b59a
Kimathi isaac <Isaackimathi@gmail.com>
2023-08-19 20:31:49 +0300

    python

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    #computes the square value of all integers of a matrix.
+    # computes the square value of all integers of a matrix.
     return [list(map((lambda x: x * x), elm)) for elm in matrix]
---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    # converts a Roman numeral to an integer and returns an integer
+    if type(roman_string) is not str or not roman_string:
+        return 0
+    roman_digt = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+            }
+    intg = [roman_digt[x] for x in roman_string]
+    res = 0
+    for x in range(len(intg)):
+        res += intg[x]
+        if intg[x - 1] < intg[x] and x != 0:
+            res -= (intg[x - 1] + intg[x -1])
+        return res
