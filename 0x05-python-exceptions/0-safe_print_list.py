8c943d40939c31eb5e3b41e85afab77c1e25e190
Kimathi isaac <Isaackimathi@gmail.com>
2023-08-28 20:59:51 +0300

    python exception

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
+    total = 0
     try:
         for n in range(x):
             print(my_list[n], end="")
+            total += 1
     except IndexError:
         pass
     finally:
         print()
-        return min(x, len(my_list))
+    return (total)
