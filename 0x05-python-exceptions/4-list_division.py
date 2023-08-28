dc5eae82a4c064008527992f684f2fa5852d3367
Kimathi isaac <Isaackimathi@gmail.com>
2023-08-28 23:21:53 +0300

    python exception

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -9,21 +9,19 @@ def list_division(my_list_1, my_list_2, list_length):
     Returns:
         A new list of length list_length containing all the divisions.
     """
-    new = []
-    try:
-        for n in range(list_length):
-            try:
-                res = my_list_1[n] / my_list_2[n]
-            except ZeroDivisionError:
-                print("division by 0")
-                res = 0
-            except (TypeError, ValueError):
-                print("wrong type")
-                res = 0
-            except IndexError:
-                print("out of range")
-                res = 0
-            finally:
-                new.append(res)
-    finally:
-        return new_list
+    nw_lst = []
+    for n in range(list_length):
+        try:
+            res = my_list_1[n] / my_list_2[n]
+        except ZeroDivisionError:
+            print("division by 0")
+            res = 0
+        except TypeError:
+            print("wrong type")
+            res = 0
+        except IndexError:
+            print("out of range")
+            res = 0
+        finally:
+            nw_lst.append(res)
+    return new_list
