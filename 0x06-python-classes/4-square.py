f01f413d17a081335594e9817b5beb3e539b5bc7
Kimathi isaac <Isaackimathi@gmail.com>
2023-08-29 18:27:22 +0300

    python classes

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -6,7 +6,7 @@
 class Square:
     """
     This class represents a square.
-                """
+    """
 
     def __init__(self, size=0):
         """
@@ -23,6 +23,18 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
 
     def area(self):
         """
