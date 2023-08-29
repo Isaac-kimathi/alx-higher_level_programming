31e60cf34c662e04bc658e618bfb39c03717d0fa
Kimathi isaac <Isaackimathi@gmail.com>
2023-08-29 16:01:19 +0300

    python classes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""Defines a square """
+
+
+class Square:
+    """
+    This class represents a square.
+    """
+
+    def __init__(self, size):
+        """
+        Initializes a new square instance.
+
+        Args: size - The size of the square.
+        """
+        self.__size = size
