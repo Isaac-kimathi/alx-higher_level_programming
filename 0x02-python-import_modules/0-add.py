80cad93ab1b801835e559e40f17f341efd4377b2
Kimathi isaac <Isaackimathi@gmail.com>
2023-08-30 12:37:29 +0300

    python-hello_world

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,11 +1,1 @@
-#!/usr/bin/python3
-
-# imports the function def add(a, b): from the file add_0.py and
-# prints the result of the addition 1 + 2 = 3
-if __name__ == "__main__":
-    from add_0 import add
-
-    a = 1
-    b = 2
-
-    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+__import__("0-add")
