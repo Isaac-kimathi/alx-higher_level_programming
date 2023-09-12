c6ced0bfff1668c92ae3c77c9edf2fa8ed48cb72
Kimathi isaac <Isaackimathi@gmail.com>
2023-09-12 17:16:52 +0300

    python-inheritance

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""module for an empty class"""
+
+
+class BaseGeometry:
+    """class representing a base geometry"""
+
+    def area(self):
+        """Method not implemented"""
+        raise Exception("area() is not implement")
