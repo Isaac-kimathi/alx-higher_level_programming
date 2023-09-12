daf91c0682fde0ac7a64f2c41ea3190919273af0
Kimathi isaac <Isaackimathi@gmail.com>
2023-09-12 23:31:06 +0300

    python-input_output

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""The module defines a file-appending function."""
+
+
+def append_write(filename="", text=""):
+    """Appends a string to the end of text file and returns chars added"""
+    with open(filename, "a", encoding="utf-8") as f:
+    return f.write(text)
