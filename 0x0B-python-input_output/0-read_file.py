990bba000d51a07f49c6b41b519957d58ac593e2
Kimathi isaac <Isaackimathi@gmail.com>
2023-09-12 22:52:25 +0300

    python-input_output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """module defines a function that reads a text file and prints it out"""
+
+
 def read_file(filename=""):
     """Reads and prints the contents of a UTF8 text file"""
-     with open(filename, 'r', encoding="utf-8") as file:
-         for line in file:
-             print(line, end='')
+     with open(filename, encoding="utf-8") as file:
+        print(f.read(), end='')
