64ed5aad4503a266c607306bea3214406bee7882
Kimathi isaac <Isaackimathi@gmail.com>
2023-09-13 17:27:59 +0300

    python-input_output

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -25,3 +25,8 @@ class Student:
                 all(type(ele) == str for ele in attrs)):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of the Student instance"""
+        for k, v in json.items():
+            setattr(self, k, v)
