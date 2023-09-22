a62209a82052828842258d4f8f57239ea6d75275
Kimathi isaac <Isaackimathi@gmail.com>
2023-09-23 00:26:28 +0300

    python-almost_a_circle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This module contains a base class"""
 
+import json
 
 class Base:
     """Base class for all class created"""
@@ -13,3 +14,48 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """ returns the JSON string representation of list_dictionaries"""
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        if (type(list_dictionaries) != list or not
+                all(type(x) == dict for x in list_dictionaries)):
+            raise TypeError("list_dictionaries must be a list")
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+       """ writes the JSON string representation of list_objs to a file"""
+       filename = cls.__name__+".json"
+       with open(filename, "w") as jsonfile:
+           if list_objs is None:
+               jsonfile.write("[]")
+           else:
+               list_dicts = [o.to_dictionary() for o in list_objs]
+               jsonfile.write(Base.to_json_string(list_objs))
+    
+    @staticmethod
+    def from_json_string(json_string):
+        """returns the list of the JSON string representation json_string"""
+        json_string_list = []
+
+        if json_string is not None and json_string != '':
+            if type(json_string) != str:
+                raise TypeError("json_string must be a string")
+            json_string_list = json.loads(json_string)
+
+        return json_string_list
+
+    @classmethod
+    def create(cls, **dictionary):
+        """returns an instance with all attributes already set"""
+        if cls.__name = 'Rectangle':
+            dummy = cls(1, 1)
+
+        elif cls.__name = 'square':
+            dummy = cls(1)
+
+        dummy.update(**dictionary)
+        return dummy
