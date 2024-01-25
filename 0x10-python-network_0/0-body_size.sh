a1103070f9f9155604a7277855a0e3cda5c41e6c
Kimathi isaac <Isaackimathi@gmail.com>
2024-01-25 20:17:28 +0300

    cURl

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/python3
-# script send a http request to URL, and displays the body size of response
+#!/bin/bash
+# script send a http request, and displays the body size of response
 curl -s "$1" | wc -c
