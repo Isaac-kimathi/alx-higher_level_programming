c96c5b7879f2c319b3a7e5a209ecf6937eb7fdec
Kimathi isaac <Isaackimathi@gmail.com>
2024-01-29 15:14:58 +0300

    requests

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""script that takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user with the letter as a parameter"""
+import sys
+import requests
+
+if __name__ == "__main__":
+    ltr = "" if len(sys.argv) == 1 else sys.argv[1]
+    upload = {"q": ltr}
+
+    r = requests.post("http://0.0.0.0:50/search_user", data=upload)
+    try:
+        res = r.json()
+        if response == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(res.get("id"), res.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
