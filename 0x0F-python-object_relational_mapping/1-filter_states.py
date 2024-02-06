597cbb289837cdfec7e16bd718063a8c4474ddf9
Kimathi isaac <Isaackimathi@gmail.com>
2024-02-06 13:48:02 +0300

    python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -2,20 +2,23 @@
 """script that lists all states with a name starting with N (upper N) 
 from the database hbtn_0e_0_usa"""
 
-import MYSQLdb as db
+import MySQLdb as db
 from sys import argv
+
+"""
+Access to the database and get the states
+from the database.
+"""
 
 if __name__ == '__main__':
     db_connect = db.connect(host="localhost", port=3306,
-            user=argv[1], passwd=argv[2], db=argv[3])
-
+                                            user=argv[1], passwd=argv[2], db=argv[3])
     db_cursor = db_connect.cursor()
 
-    db_cursor.execute(
-            "SELECT * FROM states WHERE name LIKE BINARY 'N%' \
-                    ORDER BY states.id ASC")
+    db_cursor.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' \
+                ORDER BY states.id ASC")
 
-    sel_rows = db_cursor.fetchall()
+    sel_rws = db_cursor.fetchall()
 
-    for row in sel_rows:
+    for row in sel_rws:
         print(row)
