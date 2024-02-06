a9a72c71c10a064de9afebb95f95a128272b4c9a
Kimathi isaac <Isaackimathi@gmail.com>
2024-02-06 16:12:32 +0300

    python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -6,12 +6,15 @@ from sys import argv
 
 if __name__ == '__main__':
     """access and retrieve cities from hbtn_0e_4_usa  database"""
-    db_connect = db.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+    db_connect = db.connect(host="localhost", port=3306,
+            user=argv[1], passwd=argv[2], db=argv[3])
 
     with db_connect.cursor() as db_cursor:
-        db_cursor.execute("SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.states_id = states.id ORDER BY cities.id ASC")
-
-        sel_rws = db_cursor.fetchall()
+        db_cursor.execute("SELECT cities.id, cities.name, states.name \
+                            FROM cities JOIN states ON cities.state_id \
+                            = states.id ORDER BY cities.id ASC")
+    
+    sel_rws = db_cursor.fetchall()
 
     if sel_rws is not None:
         for row in sel_rws:
